d2ef640a5f8d96e173ae6aee561daa339dfaaa8c
termiks <opopjok@gmail.com>
2022-12-01 13:10:17 -0500

    day1

---
+++ day1/getElf.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", (err, data) => {
+  if (err) throw err;
+
+  var summedArray = [];
+
+  const myArray = data.toString().split(/\n\n/);
+  for (var j = 0, n = myArray.length - 1; j < n; j++) {
+    let convArray1 = myArray[j].toString().split(/\n/);
+
+    var count = 0;
+    for (var i = convArray1.length; i--; ) {
+      count += parseInt(convArray1[i]);
+    }
+    summedArray.push(count);
+  }
+  console.log("highest elf -> " + Math.max(...summedArray));
+  var top3boys = summedArray.sort().reverse().slice(3, 6);
+  console.log("top 3 values -> " + top3boys);
+  var top3sum = 0;
+  for (var i = top3boys.length; i--; ) {
+    top3sum += top3boys[i];
+  }
+  console.log("top3sum -> " + top3sum);
+});
